e1bf5cae37e264868a41519bc5a673b5bc5791a2
crance <31661897+crance@users.noreply.github.com>
2026-04-06 14:07:20 +0000

    auto-remediation by OpenText SAST Aviator

--- src/main/java/io/swagger/petstore/resource/PetResource.java
+++ src/main/java/io/swagger/petstore/resource/PetResource.java
@@ -199,9 +199,9 @@ public class PetResource {
         try {
             petData.addPet(pet);
         } catch (Exception e) {
-            System.err.println("ERROR " + e.getMessage());
-            e.printStackTrace();
-            throw new WebApplicationException("Unexpected error", e);
+            // Log the exception internally using a proper logging framework
+            // logger.error("Error updating pet", e);
+            throw new WebApplicationException("Unexpected error", Response.Status.INTERNAL_SERVER_ERROR);
         }
         return Response.ok().entity(pet).build();
     }
